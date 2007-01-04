00ba8561f2cb6cbde9f31f52b1b25e14633fce13
Andy Stewart <boss@airbladesoftware.com>
2007-01-04 16:31:48 +0000

    Version 0.1.2.  Caches and works with stylesheet_link_tag helper.

--- lib/css_dryer.rb
+++ lib/css_dryer.rb
@@ -17,7 +17,7 @@ end
 # Converts DRY stylesheets into normal CSS ones.
 module CssDryer
 
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 
   class StyleHash < Hash  #:nodoc:
     attr_accessor :multiline
