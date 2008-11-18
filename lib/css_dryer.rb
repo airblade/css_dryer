a60125f7a7a41aaa6436270406cff35856a6f4d9
Andy Stewart <boss@airbladesoftware.com>
2008-11-18 13:51:57 +0000

    Revert "Only include StylesheetsHelper if it is defined."
    This reverts commit 963be230b307f95fe367f0b5f46c922bc72e3ddb.

--- lib/css_dryer.rb
+++ lib/css_dryer.rb
@@ -334,7 +334,7 @@ module CssDryer
   class NcssHandler
     include CssDryer
     include ERB::Util
-    include StylesheetsHelper if defined? StylesheetsHelper
+    include StylesheetsHelper
 
     cattr_accessor :erb_trim_mode
     self.erb_trim_mode = '-'
