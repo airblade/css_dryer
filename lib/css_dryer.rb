airblade/css_dryer

commit 4dc0f9278ee29af2215ea4a62edb6ea9cd883383

Rails 2.2 compatibility.

lib/css_dryer.rb
@@ -17,7 +17,7 @@ end
 # Converts DRY stylesheets into normal CSS ones.
 module CssDryer
 
-  VERSION = '0.2.8'
+  VERSION = '0.3.0'
 
   class StyleHash < Hash  #:nodoc:
     attr_accessor :multiline
@@ -334,34 +334,17 @@ module CssDryer
   class NcssHandler
     include CssDryer
     include ERB::Util
+    include StylesheetsHelper
 
-    def initialize(view)
-      @view = view
+    cattr_accessor :erb_trim_mode
+    self.erb_trim_mode = '-'
+
+    def compile(template)
+      @dry_css = ''
+      ::ERB.new(template.source, nil, erb_trim_mode, '@dry_css').result(binding)
+      css = process @dry_css
+      src = ::ERB.new("<% __in_erb_template=true %>#{css}", nil, erb_trim_mode, '@output_buffer').src
+      RUBY_VERSION >= '1.9' ? src.sub(/\A#coding:.*\n/, '') : src
     end
-
-    def compilable?
-      false
-    end
-
-    def render(template)
-      # Make local variables available to partials.
-      template.locals.each do |k,v|
-        code = "def #{k}\n"
-        code << case v
-                when String then %Q{'#{v}'}
-                when nil    then  "''"
-                else "#{v}"
-                end
-        code << "\nend\n"
-        @view.instance_eval code
-      end
-
-      dry_css = ::ERB.new(template.source, nil, @view.erb_trim_mode).result(@view.send(:binding))
-      # This processing step, where we un-nest the stylesheet, is the reason we
-      # render the template rather than compile it.  Compilation would not allow
-      # us to get at the evaluated ERB and then un-nest it.
-      process(dry_css)
-    end
-
   end
 end

README.markdown
@@ -91,14 +91,14 @@ Please also note that commas in comments will sometimes be replaced with a space
 
 You may use partial nested stylesheets as you would with normal templates.  For example, assuming your controller(s) set the @user variable and a User has a background colour (red):
 
-app/views/stylesheets/site.ncss:
+app/views/stylesheets/site.css.ncss:
 
     body {
       color: blue;
       <%= render :partial => 'content', :locals => {:background => @user.background} %>
     }
 
-app/views/stylesheets/_content.ncss:
+app/views/stylesheets/_content.css.ncss:
 
     div#content {
       background: <%= background %>;
@@ -122,7 +122,7 @@ Browser hacks are an ugly necessity in any non-trivial stylesheet.  They clutter
 
 So encapsulate them in the StylesheetsHelper instead.  Separate your lovely CSS from the decidely unlovely hacks.  For example:
 
-app/views/stylesheets/site.ncss:
+app/views/stylesheets/site.css.ncss:
 
     <% ie7 do %>
       #sidebar {
@@ -142,7 +142,7 @@ You don't have to limit yourself to browser hacks.  Consider self-clearing: to m
 
 We can do better:
 
-app/views/stylesheets/site.ncss:
+app/views/stylesheets/site.css.ncss:
 
     <%= self_clear 'div.foo', 'div.bar', 'baz' %>
 
@@ -183,7 +183,7 @@ You should see this output:
 
 You put your stylesheets, DRY or otherwise, in `app/views/stylesheets/`.  Once rendered they will be cached in `public/stylesheets/`.
 
-DRY stylesheet files should have a `ncss` extension -- think 'n' for nested.  For example, `site.ncss`.
+DRY stylesheet files should have a `ncss` extension -- think 'n' for nested.  For example, `site.css.ncss`.
 
 Get them in your views with a `css` extension like this:
 
@@ -198,13 +198,12 @@ or with Rails' `stylesheet_link_tag` helper:
 
 * Make CssDryer work with Rails' asset packaging: incorporate Dan Walters' code on GitHub.
 * Replace regexp-based nested-stylesheet parser with a Treetop parser.
-* Use .css.ncss naming convention.
 * Package as a gem as well as a plugin.
 * Configuration, e.g. `#implicit_nested_divs = true`
 * Merb compatibility.
 * Split out a separate EXAMPLES document.
 * Rake task to generate and write .css files from .ncss ones.
-* Port to Java/JSP/Servlet to bring a little ray of sunshine to J2EE webapp programming..
+* Port to Java/JSP/Servlet to bring a little ray of sunshine to J2EE webapp programming.
 
 
 ## Alternatives

generators/css_dryer/css_dryer_generator.rb
@@ -11,7 +11,7 @@ class CssDryerGenerator < Rails::Generator::Base
              :collision => :skip
       m.directory 'app/views/stylesheets'
       m.file 'test.ncss',
-             'app/views/stylesheets/test.ncss',
+             'app/views/stylesheets/test.css.ncss',
              :chmod => 0644,
              :collision => :skip
     end

generators/css_dryer/templates/stylesheets_helper.rb
@@ -63,8 +63,8 @@ module StylesheetsHelper
   # css_dryer will then de-nest the result when it
   # post-processes the result of the ERB evaluation.
   def wrap(with, &block)
-    concat "#{with} {", block.binding
+    @dry_css << "#{with} {"
     yield
-    concat "}\n", block.binding
+    @dry_css << '}'
   end
 end

commit 9fb25d2decba29cd6d01a594baca70a4b666a0de

Fix rendering partials no longer working (appears to have been broken in http://github.com/airblade/css_dryer/commit/4dc0f9278ee29af2215ea4a62edb6ea9cd883383 when def render was removed).  I'm unsure if this fix is the best way to go, but it addresses the immediate problem

lib/css_dryer.rb
@@ -331,22 +331,15 @@ module CssDryer
   # as a new templating system.
   #
   # DRY stylesheets are piped through ERB and then CssDryer#process.
-  class NcssHandler
+  class NcssHandler < ActionView::TemplateHandlers::ERB
     include CssDryer
-    include ERB::Util
     include StylesheetsHelper
 
-    cattr_accessor :erb_trim_mode
-    self.erb_trim_mode = '-'
-
-    def self.call(template)
-      new.compile(template)
+    def compile_with_css_dryer(template)
+      temp = compile_without_css_dryer(template)
+      temp + "; @output_buffer = CssDryer::NcssHandler.new.process(@output_buffer)"
     end
+    alias_method_chain :compile, :css_dryer
 
-    def compile(template)
-      @dry_css = ''
-      ::ERB.new(template.source, nil, erb_trim_mode, '@dry_css').result(binding)
-      process(@dry_css).inspect
-    end
   end
 end