3d795f7cfd0dfe47a2fd4be237f36e856a774bd7
Andy Stewart <boss@airbladesoftware.com>
2008-12-12 12:12:09 +0000

    Fixed bug where installation hook looked for nonexistent readme.

--- install.rb
+++ install.rb
@@ -1,3 +1,3 @@
 # Print installation section of README.
-readme = IO.read(File.join(File.dirname(__FILE__), 'README'))
+readme = IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
 puts readme[/(## Installation.*?)##/m, 1]
