ebc34dbe97afcc19a7746a652eb1af3594234caf
Andy Stewart <boss@airbladesoftware.com>
2009-04-11 18:29:55 +0100

    Require ERB explicitly for when using outside Rails.

--- lib/rack/css_dryer.rb
+++ lib/rack/css_dryer.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../css_dryer/processor')
+require 'ERB'
 
 # Rack::CssDryer is a Rack middleware that serves de-nested nested
 # stylesheets (.ncss) if available.
